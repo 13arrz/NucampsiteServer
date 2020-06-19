ddc8480ad65b7292ddd17d8328e689f045d889e2
13arrz <barry.mulingtapang@gmail.com>
2020-06-18 21:52:45 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '733197777455137',
+        clientSecret: '1bc53997b6ec507df761551b3938467f'
+    }
 }
